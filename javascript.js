162d680fbc3eece2092f3d2ec5f3339449138122
megafilme <119822206+megafilme@users.noreply.github.com>
2024-04-05 18:39:14 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,7 @@
+const imageUrls = [ 
+    "autodromo-foto-1-930x620.jpg",
+    "foto do chapeu da igreja.png",
+    "autodromo-foto-1-930x620.jpg",
+    // Adicione mais imagens conforme necessário
+  ];
+  
